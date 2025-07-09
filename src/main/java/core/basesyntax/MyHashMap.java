a86a82f9ebc7ffd4e57126ed44130d343fa7262b
Yevhenii <evgeniyg0801@gmail.com>
2025-07-09 21:39:47 +0300

    Added MyHashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,98 @@
 package core.basesyntax;
 
+import java.util.LinkedList;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private LinkedList<Entry<K, V>>[] buckets;
+    private int capacity;
+    private int size;
+    private V nullKeyValue;
+    private boolean hasNullKey;
+
+    public MyHashMap() {
+        this.buckets = new LinkedList[capacity];
+        this.capacity = INITIAL_CAPACITY;
+        this.hasNullKey = false;
+        this.size = 0;
+    }
+
+    private int getBucketsIndex(K key) {
+        return Math.abs(key.hashCode() % capacity);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (key == null) {
+            if (!hasNullKey) {
+                size++;
+            }
+            nullKeyValue = value;
+            hasNullKey = true;
+            return;
+        }
+        int index = getBucketsIndex(key);
+        if (buckets[index] == null) {
+            buckets[index] = new LinkedList<>();
+        }
+        for (Entry<K, V> entry : buckets[index]) {
+            if (entry.key.equals(key)) {
+                entry.value = value;
+                return;
+            }
+        }
+        buckets[index].add(new Entry<>(key, value));
+        size++;
+        if ((float) size / capacity >= LOAD_FACTOR) {
+            resize();
+        }
     }
 
     @Override
     public V getValue(K key) {
+        if (key == null) {
+            return hasNullKey ? nullKeyValue : null;
+        }
+        int index = getBucketsIndex(key);
+        if (buckets[index] != null) {
+            for (Entry<K, V> entry : buckets[index]) {
+                if (entry.key.equals(key)) {
+                    return entry.value;
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    public void resize() {
+        capacity *= 2;
+        LinkedList<Entry<K, V>>[] oldBuckets = buckets;
+        buckets = new LinkedList[capacity];
+        size = hasNullKey ? 1 : 0;
+
+        for (LinkedList<Entry<K, V>> bucket : oldBuckets) {
+            if (bucket != null) {
+                for (Entry<K, V> entry : bucket) {
+                    put(entry.key, entry.value);
+                }
+            }
+        }
     }
 }
